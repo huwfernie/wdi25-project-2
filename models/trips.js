9b6e71d8ce1d05787d6df5a0a1664bcae6f7bf39
Huw Fernie <HuwFernie@gmail.com>
2017-03-06 12:32:25 +0000

    made a basic trip model

---
+++ models/trips.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+// const legSchema = new mongoose.Schemea({
+//   images: [],
+//   words: String,
+//   when: String,
+//   where: String,
+//   who: { adults: Number, childred: Number},
+//   how: String,
+//   for: Number
+// });
+//
+// const tripSchema = new mongoose.Schema({
+//   title: {type: String, required: true  },
+//   imageHero: {type: String },
+//   images: String,
+//   season: String,
+//   date: String,
+//   duration: Number,
+//   group: { adults: Number, childred: Number},
+//   cost: String,
+//   startLocation: String,
+//   endLocation: String,
+//   method: String,
+//   favourites: [],
+//   // embeded leg report
+//   leg: [ legSchema ]
+// });
+
+const tripSchema = new mongoose.Schemea({
+  images: [],
+  words: String,
+  when: String,
+  where: String,
+  who: { adults: Number, childred: Number},
+  how: String,
+  for: Number
+});
+
+//'Trip' specifies the collection where each trip is stored.
+module.exports = mongoose.model('Trip', tripSchema);
--- models/user.js
+++ models/user.js
@@ -4,7 +4,11 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   username: {type: String },
   email: {type: String },
-  password: {type: String, required: true }
+  password: {type: String, required: true },
+  imageProfile: String,
+  imageHero: String,
+  nearestAirport: String,
+  favourites: []
 });
 
 userSchema
